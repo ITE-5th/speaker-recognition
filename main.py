247f1da243064e7b85854f70f233ad700a4dfa13
Obada <obada.jabassini@gmail.com>
2017-11-17 04:28:43 +0200

    Add multithreading

--- main.py
+++ main.py
@@ -8,66 +8,87 @@ import os
 import glob
 from sklearn.mixture import GaussianMixture
 import pickle
+from multiprocessing import Pool, cpu_count
 
 
-def train_gmms(directory):
-    gmms = dict()
+def train_dir_timit(param):
+    directory, dir_number = param
+    return train_people(directory + "DR{}".format(dir_number))
+
+
+def train_person(person_dir):
+    print(person_dir)
     components = 32
-    directory += "/TRAIN/"
-    for d in range(1, 9):
-        current_dir = directory + "DR{}/".format(d)
-        speakers = os.listdir(current_dir)
-        for speaker in speakers:
-            speaker_dir = current_dir + speaker + "/"
-            print(speaker_dir)
-            gmm = GaussianMixture(components)
-            voices = glob.glob(speaker_dir + "*.WAV")
-            # just to test the system
-            voices = [v for v in voices if "SA1" not in v]
-            mfccs = []
-            for voice in voices:
-                temp = speaker_dir + "temp.wav"
-                raw = speaker_dir + "t.rawaudio"
-                shutil.move(voice, temp)
-                call(['sox', temp, voice])
-                shutil.move(temp, raw)
-                (rate, sig) = wav.read(voice)
-                mfcc_feat = mfcc(sig, rate)
-                mfccs.append(mfcc_feat)
-            mfccs = np.vstack(mfccs)
-            gmm.fit(mfccs)
-            gmms[speaker] = gmm
+    gmm = GaussianMixture(components)
+    person_dir += "/"
+    voices = glob.glob(person_dir + "*.WAV")
+    mfccs = []
+    for voice in voices:
+        temp = person_dir + "temp.wav"
+        raw = person_dir + "t.rawaudio"
+        shutil.move(voice, temp)
+        call(['sox', temp, voice])
+        shutil.move(temp, raw)
+        (rate, sig) = wav.read(voice)
+        mfcc_feat = mfcc(sig, rate)
+        mfccs.append(mfcc_feat)
+        # os.system("rm -f {}".format(raw))
+        # os.system("rm -f {}".format(temp))
+    mfccs = np.vstack(mfccs)
+    gmm.fit(mfccs)
+    return gmm
+
+
+def train_people(people_dir):
+    names = os.listdir(people_dir)
+    gmms = {name: train_person(people_dir + "/" + name) for name in names}
     return gmms
 
 
-def test_gmms(gmms, voice_path):
-    max_score = -1000000
-    target_speaker = None
-    (rate, sig) = wav.read(voice_path)
-    mfcc_feat = mfcc(sig, rate)
-    for speaker, gmm in gmms.items():
-        scores = gmm.score_samples(mfcc_feat)
-        score = scores.mean()
-        if score > max_score:
-            max_score = score
-            target_speaker = speaker
-    return target_speaker
+def train_gmms_from_timit(directory):
+    directory += "/TRAIN/"
+    with Pool(cpu_count()) as p:
+        result = p.map(train_dir_timit, [(directory, i) for i in range(1, 9)])
+    gmms = dict()
+    for res in result:
+        gmms.update(res)
+    return gmms
 
 
-def store_gmms(directory):
-    gmms = train_gmms(directory)
-    with open('gmms.pkl', 'wb') as f:
+def detect(gmms, voice_paths):
+    result = []
+    for voice_path in voice_paths:
+        max_score = -1e6
+        target_speaker = None
+        (rate, sig) = wav.read(voice_path)
+        mfcc_feat = mfcc(sig, rate)
+        for speaker, gmm in gmms.items():
+            scores = gmm.score_samples(mfcc_feat)
+            score = scores.mean()
+            print("{} = {}".format(speaker, score))
+            if score > max_score:
+                max_score = score
+                target_speaker = speaker
+        result.append(target_speaker)
+    return result
+
+
+def store_gmms(directory, model_path):
+    gmms = train_gmms_from_timit(directory)
+    with open(model_path, 'wb') as f:
         pickle.dump(gmms, f)
 
 
-def load_gmms():
-    with open("gmms.pkl", "rb") as f:
+def load_gmms(model_path):
+    with open(model_path, "rb") as f:
         return pickle.load(f)
 
 
 if __name__ == '__main__':
-    store_gmms("./data")
-    # gmms = load_gmms()
-    # voice_path = "./data/TRAIN/DR8/MMWS0/SA1.WAV"
-    # speaker = test_gmms(gmms, voice_path)
-    # print(speaker)
+    model_path = "gmms.pkl"
+    store_gmms("./data", model_path)
+    # uncomment the following lines after training
+    # gmms = load_gmms(model_path)
+    # voice_to_test_paths = []
+    # speakers = detect(gmms, voice_to_test_paths)
+    # print(speakers[0])
